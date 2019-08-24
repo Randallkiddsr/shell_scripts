816643c1a478b91a14da8d6838862df4fd3b6b4d
Karthik <karthik2@gmx.com>
2019-08-24 07:42:41 -0700

    Added first exercise for chapter 2

---
+++ chapter_2/intro_to_exit_status.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Write a shell script that displays "This script will exit with a 0 exit 
+# staus." Be sure that the script does indeed exit with a 0 status
+
+echo "This script will exit with a 0 status"
+exit 0
