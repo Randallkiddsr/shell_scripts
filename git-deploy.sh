9fc2812e167bd7d78c2733316367c656ad8de5f5
Karthik <karthik2@gmx.com>
2019-09-22 13:02:32 -0700

    added some more comments

--- git-deploy.sh
+++ git-deploy.sh
@@ -1,7 +1,12 @@
 #!/bin/bash -ex
 
 # This script sequentially executes the usual steps of add, commit, push
-# in a git workflow. The script will stop if an error occurs
+# in a git workflow. The script will stop if an error occurs. To make this 
+# into a command place this script in your `/usr/bin` folder. 
+
+# Remember to make it an executable by  running the following command on command line:
+#  `chmod 755 name_of_script.sh` where `name_of_script.sh` is the absolute or 
+# relative path to the shell script.
 
 function deploy(){
 
